942b558e2a17df542b2f75706d7a878842f51c9b
NilayRai <nilaykumarrai0@gmail.com>
2019-01-17 13:47:16 +0530

    Code added

---
+++ src/main/java/com/example/demo/ThreadDemo.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class ThreadDemo extends Thread {
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		ThreadDemo t = new ThreadDemo();
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello");
+		}
+	}
+}
