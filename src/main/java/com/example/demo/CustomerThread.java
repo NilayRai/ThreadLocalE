a5766d977719f78c73b762eab1054f410d7e2e24
NilayRai <40742612+NilayRai@users.noreply.github.com>
2019-01-18 22:09:19 +0530

    Update CustomerThread.java

--- src/main/java/com/example/demo/CustomerThread.java
+++ src/main/java/com/example/demo/CustomerThread.java
@@ -22,7 +22,7 @@ public class CustomerThread extends Thread {
 		CustomerThread c4 = new CustomerThread("CustomerThread-4");
 		CustomerThread c5 = new CustomerThread("CustomerThread-5");
 		CustomerThread c6 = new CustomerThread("CustomerThread-6");
-
+int k=10;
 		c1.start();
 		c2.start();
 		c3.start();
