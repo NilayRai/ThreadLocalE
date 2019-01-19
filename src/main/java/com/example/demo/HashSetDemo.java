37ff3179f37288d82ddb482f85b29193468d52a5
NilayRai <nilaykumarrai0@gmail.com>
2019-01-19 23:26:22 +0530

    done

--- src/main/java/com/example/demo/HashSetDemo.java
+++ src/main/java/com/example/demo/HashSetDemo.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class HashSetDemo {
 	public static void main(String[] args) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int k=24;
+		map.put(23, 25);
+		map.put(k, 25);
 		HashSet<Integer> l = new HashSet<Integer>(5);
 		l.add(12);
 		l.add(24);
