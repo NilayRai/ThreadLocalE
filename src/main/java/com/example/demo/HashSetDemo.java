437e2d1500fb39d419b70b94818d9166a2a69378
NilayRai <nilaykumarrai0@gmail.com>
2019-01-19 10:19:01 +0530

    dfd

--- src/main/java/com/example/demo/HashSetDemo.java
+++ src/main/java/com/example/demo/HashSetDemo.java
@@ -4,9 +4,10 @@ import java.util.HashSet;
 
 public class HashSetDemo {
 	public static void main(String[] args) {
-		HashSet<Integer> l = new HashSet<Integer>();
+		HashSet<Integer> l = new HashSet<Integer>(7);
 		l.add(12);
 		l.add(24);
+		l.add(40);
 		l.add(45);
 	}
 }
