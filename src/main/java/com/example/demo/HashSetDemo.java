e0e743dd6ff064c9d3e1aa794f1c8d140a97a9c8
NilayRai <nilaykumarrai0@gmail.com>
2019-01-19 09:55:15 +0530

    dfdf

---
+++ src/main/java/com/example/demo/HashSetDemo.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+	public static void main(String[] args) {
+		HashSet<Integer> l = new HashSet<Integer>();
+		l.add(12);
+		l.add(24);
+		l.add(45);
+	}
+}
