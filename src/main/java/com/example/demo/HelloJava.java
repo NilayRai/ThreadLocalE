d21c44773390ca4fc757abb120dea9920dc064a1
NilayRai <nilaykumarrai0@gmail.com>
2019-04-10 21:53:14 +0530

    fgfg

--- src/main/java/com/example/demo/HelloJava.java
+++ src/main/java/com/example/demo/HelloJava.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-System.out.println("Hello Nilay");
+		System.out.println("Hello Nilay");
+		System.out.println();
 	}
 }
